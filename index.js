88ca2a28df831d54e489aaebcf4a931f509f6b2f
Seas <sergioaponte52@gmail.com>
2022-06-17 10:17:26 -0500

    used programed uglify

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let run = function(a){
-    console.log(a);
-}
-
-run("Hola mundo");
+var UglifyJS = require("uglify-js");
+var code = "function add(first, second) { return first + second; }";
+var result = UglifyJS.minify(code,{ compress: false, mangle: true });
+console.log(result.error); // runtime error, or `undefined` if no error
+console.log(result.code);  // minified output: function add(n,d){return n+d}
